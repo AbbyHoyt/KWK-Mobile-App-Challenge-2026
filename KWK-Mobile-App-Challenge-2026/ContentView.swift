7167537dfb408b6121d601c1aa3be839cc92ae72
AbbyHoyt <abby.hoyt@comcast.net>
2026-07-03 00:32:23 -0700

    Made basic layout of home page, added custom color palette, made blank pages to fill in later.

--- KWK-Mobile-App-Challenge-2026/ContentView.swift
+++ KWK-Mobile-App-Challenge-2026/ContentView.swift
@@ -8,29 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Define color palette.
+    // let white = Color(red: 248 / 255.0, green: 255 / 255.0, blue: 229 / 255.0)
+    // let pink = Color(red: 239 / 255.0, green: 71 / 255.0, blue: 111 / 255.0)
+    // let yellow = Color(red: 255 / 255.0, green: 196 / 255.0, blue: 61 / 255.0)
+    // let green = Color(red: 6 / 255.0, green: 214 / 255.0, blue: 160 / 255.0)
+    // let teal = Color(red: 27 / 255.0, green: 154 / 255.0, blue: 170 / 255.0)
+    
+    let white = Color(red: 232 / 255.0, green: 237 / 255.0, blue: 242 / 255.0)
+    let gray = Color(red: 44 / 255.0, green: 57 / 255.0, blue: 71 / 255.0)
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Hello, World!")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.green)
-                Image("flowers")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                Link(destination: URL(string: "https://www.kodewithklossy.com")!) {
-                    Text("Kode With Klossy Website")
-                }
+            ZStack {
+                Color(white)
+                    .ignoresSafeArea()
                 
-                NavigationLink(destination: HomeView()) {
-                    Text("Go to Home Page!")
+                VStack {
+                    Image("logo")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(30)
+                        .padding(.horizontal, 50)
+                    
+                    Text("App Name Here")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(gray)
+                        .padding(.bottom, 30)
+                    
+                    NavigationLink(destination: HomeView()) {
+                        Text("Get Started")
+                            .fontWeight(.bold)
+                            .foregroundColor(white)
+                            .padding(15)
+                            .background(gray)
+                            .cornerRadius(10)
+                    }
                 }
             }
-            .padding()
         }
-        .navigationTitle("Landing")
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarHidden(true)
     }
 }
 
---
+++ KWK-Mobile-App-Challenge-2026/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  KWK-Mobile-App-Challenge-2026
+//
+//  Created by Abby Hoyt on 7/3/26.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryView()
+}
