8159ff1014cdf01485fd8ec2d6980b23d3e7abff
AbbyHoyt <abby.hoyt@comcast.net>
2026-07-03 18:07:23 -0700

    Implemented library page with some sample colors.

--- KWK-Mobile-App-Challenge-2026/ContentView.swift
+++ KWK-Mobile-App-Challenge-2026/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     // Define color palette.    
     let white = Color(red: 232 / 255.0, green: 237 / 255.0, blue: 242 / 255.0)
     let gray = Color(red: 44 / 255.0, green: 57 / 255.0, blue: 71 / 255.0)
--- KWK-Mobile-App-Challenge-2026/LibraryView.swift
+++ KWK-Mobile-App-Challenge-2026/LibraryView.swift
@@ -11,6 +11,7 @@ struct LibraryView: View {
     // Define color palette.    
     let white = Color(red: 232 / 255.0, green: 237 / 255.0, blue: 242 / 255.0)
     let gray = Color(red: 44 / 255.0, green: 57 / 255.0, blue: 71 / 255.0)
+    let blue = Color(red: 82 / 255.0, green: 122 / 255.0, blue: 149 / 255.0)
     
     var body: some View {
         NavigationStack {
@@ -27,8 +28,8 @@ struct LibraryView: View {
                             .cornerRadius(150)
                             .padding(.horizontal, 20)
                         
-                        Text("Color Keeper")
-                            .font(.title)
+                        Text("COLOR KEEPER")
+                            .font(.custom("Anton", size: 35))
                             .fontWeight(.bold)
                             .foregroundColor(gray)
                         
@@ -39,12 +40,122 @@ struct LibraryView: View {
                         .frame(width: .infinity)
                         .frame(height: 0.5)
                         .overlay(gray)
+                        .padding(.top, -10)
                     
-                    Text("Library page.")
+                    Text("LIBRARY")
+                        .font(.custom("Anton", size: 28))
+                        .fontWeight(.bold)
                         .padding(20)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .fontWeight(.bold)
                         .foregroundColor(gray)
+                        .padding(.top, -15)
+                        .padding(.bottom, -20)
+                    
+                    HStack {
+                        VStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 75, height: 75)
+                                .foregroundColor(Color.red)
+                            
+                            Text("Cherry Red")
+                                .font(.custom("Cabin", size: 22))
+                                .fontWeight(.bold)
+                                .foregroundColor(gray)
+                            
+                            Text("#summer, #neon")
+                                .font(.custom("Cabin", size: 18))
+                                .fontWeight(.bold)
+                                .foregroundColor(blue)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding(.trailing, 20)
+                        .frame(width: 175)
+                        
+                        Spacer()
+                        
+                        Text("Placeholder text goes here!")
+                            .font(.custom("Cabin", size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(gray)
+                            .frame(width: 175)
+                    }
+                    .padding(20)
+                    
+                    HStack {
+                        VStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 75, height: 75)
+                                .foregroundColor(Color.yellow)
+                            
+                            Text("Lemon Yellow")
+                                .font(.custom("Cabin", size: 22))
+                                .fontWeight(.bold)
+                                .foregroundColor(gray)
+                            
+                            Text("#warm, #inviting, #nature")
+                                .font(.custom("Cabin", size: 18))
+                                .fontWeight(.bold)
+                                .foregroundColor(blue)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding(.trailing, 20)
+                        .frame(width: 175)
+                        
+                        Spacer()
+                        
+                        Text("Placeholder text goes here!")
+                            .font(.custom("Cabin", size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(gray)
+                            .frame(width: 175)
+                    }
+                    .padding(20)
+                    
+                    HStack {
+                        VStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 75, height: 75)
+                                .foregroundColor(Color.teal)
+                            
+                            Text("Ocean Blue")
+                                .font(.custom("Cabin", size: 22))
+                                .fontWeight(.bold)
+                                .foregroundColor(gray)
+                            
+                            Text("#cool-tone, #blue, #summer")
+                                .font(.custom("Cabin", size: 18))
+                                .fontWeight(.bold)
+                                .foregroundColor(blue)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding(.trailing, 20)
+                        .frame(width: 175)
+                        
+                        Spacer()
+                        
+                        Text("Placeholder text goes here!")
+                            .font(.custom("Cabin", size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(gray)
+                            .frame(width: 175)
+                    }
+                    .padding(20)
+                    
+//                    VStack {
+//                        RoundedRectangle(cornerRadius: 15)
+//                            .frame(width: 75, height: 75)
+//                            .foregroundColor(Color.red)
+//                        
+//                        Text("Cherry Red")
+//                            .font(.custom("Cabin", size: 22))
+//                            .fontWeight(.bold)
+//                            .foregroundColor(gray)
+//                        
+//                        Text("#summer, #neon")
+//                            .font(.custom("Cabin", size: 18))
+//                            .fontWeight(.bold)
+//                            .foregroundColor(blue)
+//                    }
                     
                     Spacer()
                 }
