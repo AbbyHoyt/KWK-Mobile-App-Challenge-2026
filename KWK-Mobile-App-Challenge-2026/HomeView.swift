46d189c75ac8f2d224eee254ca521beb740e09a8
AbbyHoyt <abby.hoyt@comcast.net>
2026-07-04 01:00:44 -0700

    Adding colors, names, descriptions, and tags is functional now!

--- KWK-Mobile-App-Challenge-2026/HomeView.swift
+++ KWK-Mobile-App-Challenge-2026/HomeView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+struct ColorItem: Identifiable {
+    let id = UUID()
+    var name: String
+    var selection: Color
+    var description: String
+    var tags: String
+}
+
 struct HomeView: View {
     // Define color palette.
     let white = Color(red: 232 / 255.0, green: 237 / 255.0, blue: 242 / 255.0)
@@ -17,12 +25,15 @@ struct HomeView: View {
     @State private var colorDescription: String = ""
     @State private var colorTags: String = ""
     
-    enum colorDetails {
-        case text(String)
-        case color(Color)
-    }
+    @State private var items: [ColorItem] = []
+    @State private var itemCount = 1
     
-    @State private var userColor: [colorDetails] = []
+//    enum colorDetails {
+//        case text(String)
+//        case color(Color)
+//    }
+//    
+//    @State private var userColor: [colorDetails] = []
     
     var body: some View {
         NavigationStack {
@@ -30,139 +41,177 @@ struct HomeView: View {
                 Color(white)
                     .ignoresSafeArea()
                 
-                VStack {
-                    HStack(alignment: .center) {
-                        Image("logo-new")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 70, height: 70)
-                            .cornerRadius(150)
-                            .padding(.horizontal, 20)
+                ScrollView {
+                    VStack {
+                        HStack(alignment: .center) {
+                            Image("logo-new")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 70, height: 70)
+                                .cornerRadius(150)
+                                .padding(.horizontal, 20)
+                            
+                            Text("COLOR KEEPER")
+                                .font(.custom("Anton", size: 35))
+                                .fontWeight(.bold)
+                                .foregroundColor(gray)
+                            
+                            Spacer()
+                        }
                         
-                        Text("COLOR KEEPER")
-                            .font(.custom("Anton", size: 35))
+                        Divider()
+                            .frame(width: .infinity)
+                            .frame(height: 0.5)
+                            .overlay(gray)
+                            .padding(.top, -10)
+                        
+                        Text("ADD A COLOR")
+                            .font(.custom("Anton", size: 28))
+                            .fontWeight(.bold)
+                            .padding(20)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundColor(gray)
+                            .padding(.top, -15)
+                        
+                        HStack(spacing: 0) {
+                            Text("Name:")
+                                .font(.custom("Cabin", size: 22))
+                                .fontWeight(.bold)
+                                .foregroundColor(gray)
+                                .padding(.leading, 20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            TextField("e.g., Lemon Yellow", text: $colorName)
+                                .frame(width: 300, height: 45)
+                                .textFieldStyle(.roundedBorder)
+                                .padding(.trailing, 20)
+                        }
+                        .padding(.top, -15)
+                        
+                        ColorPicker("Select a Color:", selection: $colorSelection, supportsOpacity: true)
+                            .font(.custom("Cabin", size: 22))
                             .fontWeight(.bold)
                             .foregroundColor(gray)
+                            .padding(.horizontal, 20)
+                            .padding(.top, 10)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                         
-                        Spacer()
-                    }
-                    
-                    Divider()
-                        .frame(width: .infinity)
-                        .frame(height: 0.5)
-                        .overlay(gray)
-                        .padding(.top, -10)
-                    
-                    Text("ADD A COLOR")
-                        .font(.custom("Anton", size: 28))
-                        .fontWeight(.bold)
-                        .padding(20)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .foregroundColor(gray)
-                        .padding(.top, -15)
-                    
-                    HStack(spacing: 0) {
-                        Text("Name:")
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(colorSelection)
+                            .frame(width: 375, height: 125)
+                            .padding(.horizontal, 20)
+                            .padding(.top, 5)
+                        
+                        Text("Description:")
                             .font(.custom("Cabin", size: 22))
                             .fontWeight(.bold)
                             .foregroundColor(gray)
                             .padding(.leading, 20)
+                            .padding(.top, 10)
                             .frame(maxWidth: .infinity, alignment: .leading)
                         
-                        TextField("e.g., Lemon Yellow", text: $colorName)
-                            .frame(width: 300, height: 45)
+                        TextField("e.g., Saw this color at the park!", text: $colorDescription)
+                            .frame(width: 375, height: 45)
                             .textFieldStyle(.roundedBorder)
+                            .padding(.horizontal, 20)
+                            .padding(.top, -10)
+                        
+                        Text("Tags:")
+                            .font(.custom("Cabin", size: 22))
+                            .fontWeight(.bold)
+                            .foregroundColor(gray)
+                            .padding(.leading, 20)
+                            .padding(.top, 10)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        TextField("e.g., #summer, #neon", text: $colorTags)
+                            .frame(width: 375, height: 45)
+                            .textFieldStyle(.roundedBorder)
+                            .padding(.horizontal, 20)
+                            .padding(.bottom, 10)
+                            .padding(.top, -10)
+                        
+                        HStack {
+                            Spacer()
+                            
+                            Button("Submit Color") {
+                                addColorItem()
+    //                            userColor.append(.text(colorName))
+    //                            userColor.append(.color(colorSelection))
+    //                            userColor.append(.text(colorDescription))
+    //                            userColor.append(.text(colorTags))
+    
+                                colorName = ""
+                                colorSelection = Color.pink
+                                colorDescription = ""
+                                colorTags = ""
+                            }
+                            .font(.custom("Cabin", size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(white)
+                            .padding(10)
+                            .background(gray)
+                            .cornerRadius(10)
                             .padding(.trailing, 20)
-                    }
-                    .padding(.top, -15)
-                    
-                    ColorPicker("Select a Color:", selection: $colorSelection, supportsOpacity: true)
-                        .font(.custom("Cabin", size: 22))
-                        .fontWeight(.bold)
-                        .foregroundColor(gray)
-                        .padding(.horizontal, 20)
-                        .padding(.top, 10)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(colorSelection)
-                        .frame(width: 375, height: 125)
-                        .padding(.horizontal, 20)
-                        .padding(.top, 5)
-                    
-                    Text("Description:")
-                        .font(.custom("Cabin", size: 22))
-                        .fontWeight(.bold)
-                        .foregroundColor(gray)
-                        .padding(.leading, 20)
-                        .padding(.top, 10)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    TextField("e.g., Saw this color at the park!", text: $colorDescription)
-                        .frame(width: 375, height: 45)
-                        .textFieldStyle(.roundedBorder)
-                        .padding(.horizontal, 20)
-                        .padding(.top, -10)
-                    
-                    Text("Tags:")
-                        .font(.custom("Cabin", size: 22))
-                        .fontWeight(.bold)
-                        .foregroundColor(gray)
-                        .padding(.leading, 20)
-                        .padding(.top, 10)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    TextField("e.g., #summer, #neon", text: $colorTags)
-                        .frame(width: 375, height: 45)
-                        .textFieldStyle(.roundedBorder)
-                        .padding(.horizontal, 20)
-                        .padding(.bottom, 10)
-                        .padding(.top, -10)
-                    
-                    HStack {
+                        }
+                        .padding(.bottom, 20)
+                        
+                        Divider()
+                            .frame(width: .infinity)
+                            .frame(height: 0.5)
+                            .overlay(gray)
+                        
                         Spacer()
                         
-                        Button("Submit Color") {
-                            userColor.append(.text(colorName))
-                            userColor.append(.color(colorSelection))
-                            userColor.append(.text(colorDescription))
-                            userColor.append(.text(colorTags))
-                            
-                            colorName = ""
-                            colorSelection = Color.pink
-                            colorDescription = ""
-                            colorTags = ""
+                        NavigationLink("VIEW YOUR LIBRARY") {
+                            LibraryView(name: colorName, selection: colorSelection, description: colorDescription, tags: colorTags)
                         }
-                        .font(.custom("Cabin", size: 20))
+                        .font(.custom("Anton", size: 24))
                         .fontWeight(.bold)
                         .foregroundColor(white)
-                        .padding(10)
+                        .padding(15)
                         .background(gray)
                         .cornerRadius(10)
-                        .padding(.trailing, 20)
+                        .padding(.top, 20)
                     }
-                    .padding(.bottom, 20)
                     
-                    Divider()
-                        .frame(width: .infinity)
-                        .frame(height: 0.5)
-                        .overlay(gray)
                     
-                    Spacer()
-                    
-                    NavigationLink("VIEW YOUR LIBRARY") {
-                        LibraryView(name: colorName, selection: colorSelection, description: colorDescription, tags: colorTags)
+                    // Space to add new colors:
+                    VStack(spacing: 10) {
+                        if items.isEmpty {
+                            Text("No colors in library yet!")
+                        } else {
+                            ForEach($items) { item in
+                                Text(item.name.wrappedValue)
+                                
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(item.selection.wrappedValue)
+                                
+                                Text(item.description.wrappedValue)
+                                
+                                Text(item.tags.wrappedValue)
+                            }
+                        }
                     }
-                    .font(.custom("Anton", size: 24))
-                    .fontWeight(.bold)
-                    .foregroundColor(white)
-                    .padding(15)
-                    .background(gray)
-                    .cornerRadius(10)
+                    
+                    
                 }
+                .frame(height: nil)
             }
             .navigationBarBackButtonHidden(true)
         }
+    }
+    
+    private func addColorItem() {
+        let newItem = ColorItem(
+            name: colorName,
+            selection: colorSelection,
+            description: colorDescription,
+            tags: colorTags
+        )
+        items.append(newItem)
+        itemCount += 1
     }
 }
 
